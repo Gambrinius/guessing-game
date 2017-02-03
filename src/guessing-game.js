7cc2c052cd4157153e8e36ab1f8108fe6accdd2c
Ilya_Konon <ilya.konon.95@gmail.com>
2017-02-03 23:14:58 +0300

    fixed Math.ceil() method instead Math.floor()

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,25 +1,25 @@
 class GuessingGame {
     constructor() {
         this.left = null;
-        this.rigth = null;
+        this.right = null;
         this.middle = null;
     }
 
     //this method accepts min and max value of range of number to guess
     setRange(min, max) {
         this.left = min;
-        this.rigth = max;
+        this.right = max;
     }
 
     //this method returns solution candidate (you make an assumption based on range and previous assumptions)
     guess() {
-        this.middle = Math.floor((this.left + this.rigth) / 2);
+        this.middle = Math.ceil((this.left + this.right) / 2); // Math.ceil() round a number upward to its nearest integer.
         return this.middle;
     }
 
     //this method is called if prev call of guess() returned number which is greater than answer
     lower() {
-        this.rigth = this.middle;
+        this.right = this.middle;
     }
 
     //this method is called if prev call of guess() returned number which is lower than answer
