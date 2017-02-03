e88e642f1234add2ee7a0808f46dac876d5efd43
Ilya_Konon <ilya.konon.95@gmail.com>
2017-02-03 22:46:42 +0300

    implemented all methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = null;
+        this.rigth = null;
+        this.middle = null;
+    }
 
+    //this method accepts min and max value of range of number to guess
     setRange(min, max) {
-
+        this.left = min;
+        this.rigth = max;
     }
 
+    //this method returns solution candidate (you make an assumption based on range and previous assumptions)
     guess() {
-
+        this.middle = Math.floor((this.left + this.rigth) / 2);
+        return this.middle;
     }
 
+    //this method is called if prev call of guess() returned number which is greater than answer
     lower() {
-
+        this.rigth = this.middle;
     }
 
+    //this method is called if prev call of guess() returned number which is lower than answer
     greater() {
-
+        this.left = this.middle;
     }
 }
 
